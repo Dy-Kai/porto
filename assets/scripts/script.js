4df75f525b97822fb2d90ddc388467df879a693f
Dy-kai <dodykhairi129@gmail.com>
2025-02-14 01:43:21 +0700

    Tidying up the code, remaking image preview feature

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,8 @@
-// Initial
-window.onload = function() {
-    document.body.onselectstart = function() {
-        return false;
-    }
-}
-
-function CreateGalleryItem(imgName){
+// functions
+function CreateGalleryItems(imgName) {
     const img = document.createElement('img');
-    img.src = 'assets/img/aw/'+imgName+'.jpg';
     img.classList.add('gallery-img');
+    img.src = 'assets/img/aw/' + imgName + '.jpg';
     img.alt = imgName;
 
     const div = document.createElement('div');
@@ -20,33 +14,76 @@ function CreateGalleryItem(imgName){
     gallery.appendChild(div);
 }
 
-for (let i = 0; i < 9; i++) {
-    CreateGalleryItem(i+1);
+function CreateImgPreview(imgDir, imgAlt) {
+    const pageTop = document.createElement('div');
+    pageTop.id = 'page-top';
+
+    const imgPreview = document.createElement('div');
+    imgPreview.classList.add('img-preview', 'position-fixed', 'z-2');
+
+    const closePreview = document.createElement('div');
+    closePreview.id = 'close-preview';
+    closePreview.setAttribute("onclick", "CloseImgPreview();");
+    closePreview.classList.add('position-fixed', 'top-0', 'end-0', 'm-5');
+
+    const closeIcon = document.createElement('i');
+    closeIcon.classList.add('fa-solid', 'fa-xmark');
+    closeIcon.style.fontSize = '30px';
+
+    const img = document.createElement('img');
+    img.classList.add('position-fixed', 'top-50', 'start-50', 'translate-middle');
+    img.src = imgDir;
+    img.alt = imgAlt;
+
+    closePreview.appendChild(closeIcon);
+    imgPreview.appendChild(closePreview);
+    imgPreview.appendChild(img);
+    pageTop.appendChild(imgPreview);
+
+    const body = document.getElementById('body');
+    body.insertBefore(pageTop, body.firstChild);
 }
 
-// Update
+function OpenImgPreview() {
+    const imgWindow = document.getElementsByClassName('img-preview')[0];
+    imgWindow.style.visibility = 'visible';
+    imgWindow.classList.add('showing');
+
+    setTimeout(function () {
+        imgWindow.classList.remove('showing');
+    }, 500);
+}
+
+function CloseImgPreview() {  
+    const imgWindow = document.getElementsByClassName('img-preview')[0];
+    imgWindow.classList.add('hiding');
+
+    setTimeout(function () {
+        imgWindow.parentElement.remove();
+    }, 500);
+};
+
+// Start
+window.onload = function () {
+    document.body.onselectstart = function () {
+        return false;
+    }
+}
+
+// create gallery contents
+for (let i = 0; i < 9; i++) {
+    CreateGalleryItems(i + 1);
+}
+
+// Open image preview
 const galleryImg = document.querySelectorAll('.gallery-img');
+galleryImg.forEach(function (element) {
+    element.addEventListener('click', function (event) {
+        imgDir = event.target.src;
+        imgAlt = event.target.alt;
 
-galleryImg.forEach(function(element) {
-    element.addEventListener('click', function(event) {
+        if (!document.getElementById('page-top')) CreateImgPreview(imgDir, imgAlt);
 
-    const imgPreviewHandler = document.getElementsByClassName('img-preview')[0];
-    imgPreviewHandler.classList.add('show');
-
-    imgPreviewHandler.children[1].src = event.target.src;
-    setTimeout(function() {
-        imgPreviewHandler.classList.remove('hide');
-    }, 1000);
+        OpenImgPreview()
     })
 });
-
-
-const closeImgBtn = document.getElementById('close-preview');
-
-closeImgBtn.addEventListener('click', function(){
-    const img = document.getElementsByClassName('img-preview')[0];
-    img.classList.add('hide');
-    setTimeout(function() {
-        img.classList.remove('show');
-    }, 500);
-})
